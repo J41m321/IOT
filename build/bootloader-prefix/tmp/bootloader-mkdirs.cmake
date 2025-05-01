c14c67fc0e120b06216b420b949eecf8c194e639
Lacosabuena <114421347+nicolasg32@users.noreply.github.com>
2025-05-01 16:41:01 -0400

    init

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "C:/Users/nico_/esp/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "C:/Users/nico_/esp/esp-idf/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader"
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix"
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/tmp"
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/src"
+  "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/nico_/Desktop/Universidad/iot/C_digo_aux_3/wifi_connect/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
